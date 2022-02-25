14854cbaecb102b74ed829624a854766be859b2c
Tooba Malaika <toobachuhan92@gmail.com>
2022-02-25 22:13:07 +0500

    Add Header

---
+++ wp-custom-product-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Plugin Name: WP Custom Product Plugin
+* Plugin URI: https://codup.co/
+* Author: Tooba Malaika™
+* Version: 1.0.0
+* Requires PHP: 7.4
+* Description: Product listing catalog which will give a user the ability to browse and filter the catalog.
+* Tags: products, codup, tooba malaika, full-stack developer
+* Text Domain: wp-custom-product-plugin
+* License: GPL V3
+* Author URI: https://codup.co/
+*/
+defined('ABSPATH') || die ("You can't access this directory!");
