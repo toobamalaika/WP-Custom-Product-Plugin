87816c1f9c7cb38965bd777cbf055d3eaf8771ba
Tooba Malaika <toobachuhan92@gmail.com>
2022-02-26 02:36:38 +0500

    Add category and tag taxonomy for products

--- includes/product-listings-cpt.php
+++ includes/product-listings-cpt.php
@@ -4,7 +4,8 @@ add_action('init', 'wcpp_product_post');
 
 function wcpp_product_post() {
 
-	$labels = array(
+	// register post for product
+	$labels_post = array(
 		'name' => 'Products List',
 		'singular_name' => 'Product List',
 		'add_new' => 'Add New',
@@ -20,10 +21,11 @@ function wcpp_product_post() {
 		'parent_item_colon' => 'Parent Product List',
 		'menu_name' => 'Products List'
 	);
+
 	register_post_type('products', array(
 
 		'label' => 'Product',
-		'labels' => $labels,
+		'labels' => $labels_post,
 		'public' => true,
 		'description' => 'Description for products',
 		'supports' => ['title', 'editor', 'thumbnail', ],
@@ -41,4 +43,53 @@ function wcpp_product_post() {
 		'capability_type' => 'post',
 		'menu_icon'             => 'dashicons-welcome-widgets-menus',
 	) );
+
+	// register taxonomy for category of products
+
+	$taxonomies = array(
+		array(
+			'slug'         => 'product-categories',
+			'single_name'  => 'Category',
+			'plural_name'  => 'Categories',
+			'post_type'    => 'products',
+			'rewrite'      => array( 'slug' => 'categories' ),
+		),
+		array(
+			'slug'         => 'product-tags',
+			'single_name'  => 'Tag',
+			'plural_name'  => 'Tags',
+			'post_type'    => 'products',
+			'rewrite'      => array( 'slug' => 'tags' ),
+		),
+	);
+
+	foreach( $taxonomies as $taxonomy ) {
+		$labels = array(
+			'name' => $taxonomy['plural_name'],
+			'singular_name' => $taxonomy['single_name'],
+			'search_items' =>  'Search ' . $taxonomy['plural_name'],
+			'all_items' => 'All ' . $taxonomy['plural_name'],
+			'parent_item' => 'Parent ' . $taxonomy['single_name'],
+			'parent_item_colon' => 'Parent ' . $taxonomy['single_name'] . ':',
+			'edit_item' => 'Edit ' . $taxonomy['single_name'],
+			'update_item' => 'Update ' . $taxonomy['single_name'],
+			'add_new_item' => 'Add New ' . $taxonomy['single_name'],
+			'new_item_name' => 'New ' . $taxonomy['single_name'] . ' Name',
+			'menu_name' => $taxonomy['plural_name']
+		);
+		
+		$rewrite = isset( $taxonomy['rewrite'] ) ? $taxonomy['rewrite'] : array( 'slug' => $taxonomy['slug'] );
+		$hierarchical = isset( $taxonomy['hierarchical'] ) ? $taxonomy['hierarchical'] : true;
+
+		$labels_taxonomy = array(
+			'hierarchical' => $hierarchical,
+			'labels' => $labels,
+			'show_ui' => true,
+			'query_var' => true,
+			'rewrite' => $rewrite,
+			'public' => true,
+		);
+	
+		register_taxonomy($taxonomy['slug'], $taxonomy['post_type'], $labels_taxonomy);
+	}
 }
--- includes/product-listings-scripts.php
+++ includes/product-listings-scripts.php
@@ -6,8 +6,8 @@ function wcpp_admin_scripts() {
 
 	if ( is_admin() ) {
 
-		wp_enqueue_style('wcpp-bootstrap', plugin_dir_url( __FILE__ ). '../assets/plugin/bootstrap/css/bootstrap.css');
+		wp_enqueue_style('wcpp-bootstrap-css', plugin_dir_url( __FILE__ ). '../assets/plugin/bootstrap/css/bootstrap.css');
 		wp_enqueue_style('wcpp-custom-style', plugin_dir_url( __FILE__ ). '../assets/css/style.css');
-		// wp_enqueue_script('pl-script', plugins_url(). '/product-listings/assets/js/main.js', [ 'jquery' ]);
+		wp_enqueue_script('wcpp-bootstrap-js', plugin_dir_url( __FILE__ ). '../assets/plugin/bootstrap/js/bootstrap.js');
 	}
 }
