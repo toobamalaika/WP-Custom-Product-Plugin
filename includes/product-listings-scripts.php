6bdaa3e86c9460dc20ad9d518cabc5c4f483831f
Tooba Malaika <toobachuhan92@gmail.com>
2022-02-26 01:38:49 +0500

    Add metabox in product post

--- includes/product-listings-scripts.php
+++ includes/product-listings-scripts.php
@@ -1,1 +1,13 @@
 <?php
+
+add_action('admin_init', 'wcpp_admin_scripts');
+
+function wcpp_admin_scripts() {
+
+	if ( is_admin() ) {
+
+		wp_enqueue_style('wcpp-bootstrap', plugin_dir_url( __FILE__ ). '../assets/plugin/bootstrap/css/bootstrap.css');
+		wp_enqueue_style('wcpp-custom-style', plugin_dir_url( __FILE__ ). '../assets/css/style.css');
+		// wp_enqueue_script('pl-script', plugins_url(). '/product-listings/assets/js/main.js', [ 'jquery' ]);
+	}
+}
